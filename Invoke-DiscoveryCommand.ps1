7043f404cc0fead591dd9ecff355780235ebbf9f
Adam Migus <adam@migus.org>
2020-12-09 12:29:39 -0500

    Refactoring - Avoid defaulting to environment variables for the URL and credentials - Use Uri type for the Secret Server URL - Check if the scan is running before trying to start it - Add Debug, Informational and Verbose output

--- Invoke-DiscoveryCommand.ps1
+++ Invoke-DiscoveryCommand.ps1
@@ -3,9 +3,9 @@
     Run a Thycotic Secret Server Discovery or ComputerScan
 
 .DESCRIPTION
-    Call the Thycotic Secret Server REST API /Discovery/run endpoint to
-    run a Discovery or ComputerScan and, if successful, call the /status
-    endpoint to confirm that it is running.
+    Call the Thycotic Secret Server REST API /Discovery/status endpoint to
+    check whether a Discovery or ComputerScan is already running and if not,
+    start it by calling the /Discovery/run endpoint.
 
 
 Copyright 2020, The Migus Group, LLC. All rights reserved
@@ -14,46 +14,63 @@ Copyright 2020, The Migus Group, LLC. All rights reserved
 [CmdletBinding(DefaultParameterSetName = 'UserPass')]
 Param(
     [Parameter(Mandatory = $true, Position = 0)]
-    [ValidateScript( { $_ -in @("ComputerScan", "Discovery")} )]
-    [string]$CommandType = "Discovery",
+    [ValidateSet("ComputerScan", "Discovery")][string]$CommandType = "Discovery",
     [Parameter(Mandatory = $true, Position = 1)]
-    [string]$BaseUrl = ${env:TSS_SERVER_URL},
+    [Uri]$SecretServerUri,
     [Parameter(Mandatory = $true, ParameterSetName = "UserPass", Position = 2)]
-    [string]$Username = ${env:TSS_USERNAME},
+    [string]$Username,
     [Parameter(Mandatory = $true, ParameterSetName = "UserPass", Position = 3)]
-    [SecureString]$Password = (ConvertTo-SecureString -AsPlainText ${env:TSS_PASSWORD} -Force),
+    [SecureString]$Password,
     [Parameter(Mandatory = $true, ParameterSetName = "Credential", Position = 2)]
-    [ValidateScript( { $null -ne $_.Username } )]
     [PSCredential]$Credential
 )
 $ErrorActionPreference = 'Stop'
+$BaseUri = ${SecretServerUri}.ToString().TrimEnd('/')
 
-if ($null -ne $Credential) {
+Write-Debug "BaseUri: ${BaseUri}"
+
+if ($Credential) {
     $Username = $Credential.Username
-    $Password = $Credential.Password
+    $CleartextPassword = $Credential.GetNetworkCredential().Password
 }
+elseif ($Password) {
+    $CleartextPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+        [Runtime.InteropServices.Marshal]::SecureStringToBSTR($Password))
+}
+Write-Debug "Username: '${Username}' Password: $($CleartextPassword -replace '(.)', '*')"
 # Call the /oauth2/token endpoint, extract the access_token from the
 # response and use it to create a Bearer token Authorization header
 $headers = @{ 'Authorization' = 'Bearer ' + (
         Invoke-WebRequest -Body (@{
-                'username' = $Username;
-                'password' = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
-                    [Runtime.InteropServices.Marshal]::SecureStringToBSTR($Password)
-                )
-                'grant_type' = 'password';
-            }) -Method Post -Uri "${BaseUrl}/oauth2/token"
-        | ConvertFrom-Json).access_token
+                'username'   = $Username
+                'password'   = $CleartextPassword
+                'grant_type' = 'password'
+            }
+        ) -Method Post -Uri "${BaseUri}/oauth2/token" |
+        ConvertFrom-Json).access_token
 }
+Remove-Variable CleartextPassword
 
-# Call /Discovery/run and if it succeeds then call /Discovery/status
-$discoveryApiUrl = "${BaseUrl}/api/v1/Discovery"
+$discoveryUri = "${BaseUri}/api/v1/Discovery"
+
+Write-Verbose "Attempting to get Discovery status"
+$Status = Invoke-RestMethod -Headers $headers -Uri "${discoveryUri}/status"
+Write-Debug "Status: ${Status}"
+
+# Do nothing if Discovery is already running
+if ($CommandType -eq "Discovery" -and $Status.isDiscoveryFetchRunning -or
+    $CommandType -eq "ComputerScan" -and $Status.isDiscoveryComputerScanRunning) {
+    Write-Information "${CommandType} is already running"
+    return
+}
+Write-Verbose "Attempting to run ${CommandType}"
 # /Discovery/run returns "True" or "False", so we can cast it as boolean
 if (([boolean](
-    Invoke-RestMethod -Headers $headers -Body (@{
-        "data" = @{
-            "commandType" = $CommandType
-        }
-    } | ConvertTo-Json) -Method Post -Uri "${discoveryApiUrl}/run"
-))) {
-    Invoke-RestMethod -Headers $headers -Uri "${discoveryApiUrl}/status"
+            Invoke-RestMethod -Headers $headers -ContentType "application/json" -Body (@{
+                    "data" = @{
+                        "commandType" = $CommandType
+                    }
+                } | ConvertTo-Json) -Method Post -Uri "${discoveryUri}/run"
+        ))) {
+    Write-Information "${CommandType} started"
 }
